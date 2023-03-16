d7056ee02f684e390c105c55ab2e912175363e33
Paulo Scapol <paulo.scapol2@hotmail.com>
2023-03-16 13:31:20 -0300

    Realizado o Requisito 7

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 // https://www.w3schools.com/tags/tag_tbody.asp
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
+
     return (
       <div>
-        <table>
+        <table width="100%">
           <thead>
             <tr>
+              {/* <th>ID</th> */}
               <th>Descrição</th>
               <th>Tag</th>
               <th>Método de pagamento</th>
@@ -20,10 +25,43 @@ class Table extends Component {
               <th>Editar/Excluir</th>
             </tr>
           </thead>
+
+          <tbody>
+            { expenses.map((expense) => (
+              <tr key={ expense.id } id={ expense.id }>
+                {/* <td>{ expense.id + 1 }</td> */}
+                <td>{ expense.description }</td>
+                <td>{ expense.tag }</td>
+                <td>{ expense.method }</td>
+                <td>{ (expense.value * 1).toFixed(2) }</td>
+                <td>{ expense.exchangeRates[expense.currency].name }</td>
+                {/* <td>{ expense.exchangeRates[expense.currency].code }</td> */}
+                <td>
+                  { (expense.exchangeRates[expense.currency].ask * 1).toFixed(2) }
+                </td>
+                <td>
+                  { (expense.value * (expense.exchangeRates[expense.currency].ask))
+                    .toFixed(2) }
+                </td>
+                <td>BRL</td>
+                <td>
+                  <button className="edit-btn">Editar</button>
+                  <button className="delete-btn">Excluir</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = (state) => ({
+  ...state.wallet,
+});
+Table.propTypes = {
+  expenses: PropTypes.array,
+}.isRequired;
+
+export default connect(mapStateToProps)(Table);
